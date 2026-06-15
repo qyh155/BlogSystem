506aa5a6ccecf3a0bba93d03d16875d8f2b8294a
qyh155 <1553826362@qq.com>
2026-06-15 23:05:56 +0800

    优化了工厂模式的配置

--- src/BlogSystem.API/Program.cs
+++ src/BlogSystem.API/Program.cs
@@ -1,6 +1,10 @@
+using BlogSystem.src.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option => option.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/BlogSystem.Infrastructure/Data/AppDbContextFactory.cs
+++ src/BlogSystem.Infrastructure/Data/AppDbContextFactory.cs
@@ -9,28 +9,14 @@ namespace BlogSystem.src.Infrastructure.Data
     {
         public AppDbContext CreateDbContext(string[] args)
         {
-            var currentDir = Path.GetDirectoryName(typeof(AppDbContextFactory).Assembly.Location) ?? Directory.GetCurrentDirectory();
-            // 1. 找到 API 项目所在的目录（因为 appsettings.json 在那里）
-            var apiProjectPath = Path.GetFullPath(Path.Combine(currentDir, "../../../../BlogSystem.API"));
+            //从环境变量注入或者硬编码获得连接字符串
+            var connectionString = Environment.GetEnvironmentVariable("BLOG_CONNECTION") ?? "Host=localhost;Port=54321;Database=BlogSystem;Username=postgres;Password=123456";
+            //创建连接配置
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            //配置为Postgressql
+            optionsBuilder.UseNpgsql(connectionString);
 
-            // 2. 构建配置
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(apiProjectPath)
-                .AddJsonFile("appsettings.json", optional: false)
-                .AddJsonFile("appsettings.Development.json", optional: true)
-                .Build();
-
-            // 3. 读取连接字符串
-            var connectionString = configuration.GetConnectionString("Default");
-            //上面的模式是为了文件配置连接字符串，当然也可以直接写死连接字符串，或者通过环境变量等方式获取连接字符串。
-
-            // 4. 配置 DbContext
-            var optionsBuider = new DbContextOptionsBuilder<AppDbContext>();
-            
-            //var connectionString = "Host=localhost;Port=54321;Database=BlogSystem;Username=postgres;Password=123456";
-            optionsBuider.UseNpgsql(connectionString);
-
-            return new AppDbContext(optionsBuider.Options);
+            return new AppDbContext(optionsBuilder.Options);
         }
     }
 }
